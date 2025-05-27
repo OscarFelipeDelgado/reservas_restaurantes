41f50d2c12167c8bbc84274d208b9506ff48a4c4
Oscar Delgado <osfedeal@gmail.com>
2025-05-26 20:16:02 -0500

    .

--- front_end/src/app/app.routes.ts
+++ front_end/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { Routes } from '@angular/router';
 import {AppComponent} from './app.component'
 import {CatalogoUniversalComponent} from './catalogo-universal/catalogo-universal.component'
 import {MenuInicioComponent} from'./menu-inicio/menu-inicio.component'
-import {PersonasComponent} from'./personas/personas.component';
+import {PersonasComponent} from'./personas/personas.component'
+import { MenusComponent } from './menus/menus.component';
 
 export const routes: Routes = [
     {path:"", pathMatch: 'prefix', redirectTo: "MenuInicio"},
     {path: "Universal", component: CatalogoUniversalComponent},
     {path: "MenuInicio", component: MenuInicioComponent},
     {path: "personas", component: PersonasComponent},
-
+    {path: "menus", component: MenusComponent},
 ];
