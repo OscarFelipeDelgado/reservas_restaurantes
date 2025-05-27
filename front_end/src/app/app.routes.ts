0e58952a2d67732257d36ef6aaa6852bab9d7078
Dvanegas334 <128095615+Dvanegas334@users.noreply.github.com>
2025-05-26 20:16:24 -0500

    Reservas

--- front_end/src/app/app.routes.ts
+++ front_end/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import {AppComponent} from './app.component'
 import {CatalogoUniversalComponent} from './catalogo-universal/catalogo-universal.component'
-import {MenuInicioComponent} from'./menu-inicio/menu-inicio.component';
+import {MenuInicioComponent} from'./menu-inicio/menu-inicio.component'
+import {ReservasComponent} from'./reservas/reservas.component';
 
 export const routes: Routes = [
     {path:"", pathMatch: 'prefix', redirectTo: "MenuInicio"},
     {path: "Universal", component: CatalogoUniversalComponent},
-    {path: "MenuInicio", component: MenuInicioComponent}
+    {path: "MenuInicio", component: MenuInicioComponent}, 
+    {path: "reservas", component: ReservasComponent}
 ];
