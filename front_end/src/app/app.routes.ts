88f7b397eab790c662dbdd76b34563543c316923
Dvanegas334 <128095615+Dvanegas334@users.noreply.github.com>
2025-05-26 20:39:39 -0500

    .

--- front_end/src/app/app.routes.ts
+++ front_end/src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { Routes } from '@angular/router';
 import {AppComponent} from './app.component'
 import {CatalogoUniversalComponent} from './catalogo-universal/catalogo-universal.component'
 import {MenuInicioComponent} from'./menu-inicio/menu-inicio.component'
-import {ReservasComponent} from'./reservas/reservas.component';
+import {ReservasComponent} from'./reservas/reservas.component'
 import {PersonasComponent} from'./personas/personas.component'
-import {MenusComponent} from './menus/menus.component';
+import {MenusComponent} from './menus/menus.component'
+import {ContactosComponent} from './contactos/contactos.component'
+import {MesasComponent} from './mesas/mesas.component'
+import {MesasxmenuComponent} from './mesasxmenu/mesasxmenu.component';
 
 export const routes: Routes = [
     {path:"", pathMatch: 'prefix', redirectTo: "MenuInicio"},
@@ -14,4 +17,7 @@ export const routes: Routes = [
     {path: "reservas", component: ReservasComponent},
     {path: "personas", component: PersonasComponent},
     {path: "menus", component: MenusComponent},
+    {path: "contactos", component: ContactosComponent},
+    {path: "Mesas", component: MesasComponent},
+    {path: "MesasxMenu", component: MesasxmenuComponent},
 ];
